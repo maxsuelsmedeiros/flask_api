aa82b7db61cfecc073ce6bde843a299c269ad51b
maxsuelsmedeiros <maxsuelmedeiros@hotmail.com>
2025-07-17 23:03:20 -0300

    adding the new method to the letter counter and the logging config

--- app.py
+++ app.py
@@ -1,11 +1,42 @@
 from flask import Flask
 from src.letter_counter import TextCounter
+import logging
+import os
+#confing and adding the logger for debuging and logging purposes
+
+logger : logging.Logger = logging.getLogger(name = __name__)
+logger.setLevel(logging.DEBUG)
+format : str= '%(asctime)s -> %(levelname)s -> %(module)s -> %(message)s'
+formatter : logging.Formatter = logging.Formatter(fmt=format)
+stream_handler : logging.StreamHandler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+logger.addHandler(stream_handler)
+
+#adding file handler
+try:
+    file_handler : logging.FileHandler = logging.FileHandler('logs/api_log.log')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+except FileNotFoundError as e:
+    logger.exception(msg='Unable to create the log file,!')
+    os.mkdir('logs')
+    file_handler : logging.FileHandler = logging.FileHandler('logs/api_log.log')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+except Exception as e:
+    logger.error(msg='An unexpected error has ocurred!',exc_info=True)
 
 app : Flask = Flask(__name__)
 tc : TextCounter = TextCounter()
 @app.route('/')
-def main():
+def main() -> str:
     return 'Welcome to the Text Counter API!'
+
+#adding the character counter
+
+@app.route('/all_char_counter/')
+def chars_counter() -> str:
+    return str(tc.characters_total_count(''))
 
 if __name__=='__main__':
     main()
--- src/letter_counter.py
+++ src/letter_counter.py
@@ -1,9 +1,23 @@
 # This module reads the all the characters from the text the user sends and respond with the many details, based on the method requested
+import logging
 
 class TextCounter:
 
+    logger: logging.Logger
+
     def __init__(self) -> None:
-        pass
+        self.logger : logging.Logger = logging.getLogger(name = __name__)
+        self.logger.setLevel(logging.DEBUG)
+        format : str= '%(asctime)s -> %(levelname)s -> %(module)s -> %(message)s'
+        formatter : logging.Formatter = logging.Formatter(fmt=format)
+        stream_handler : logging.StreamHandler = logging.StreamHandler()
+        stream_handler.setFormatter(formatter)
+        self.logger.addHandler(stream_handler)
+        return
 
     def __repr__(self) -> str:
+        self.logger.debug(msg='Returning class representation.')
         return 'TextCounter'
+    
+    def characters_total_count(self, input_text : str) -> int:
+        return 0 
